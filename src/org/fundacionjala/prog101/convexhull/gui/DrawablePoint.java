d467c63370eba2155aa941b220e167238e676fa9
AbelBarrientos <abelbs27101@gmail.com>
2017-05-09 14:58:12 -0400

    Add Drawable Point

---
+++ src/org/fundacionjala/prog101/convexhull/gui/DrawablePoint.java
@@ -0,0 +1,28 @@
+package org.fundacionjala.prog101.convexhull.gui;
+
+import org.fundacionjala.prog101.convexhull.Point;
+
+import java.awt.*;
+
+/**
+ * Created by AbelBarrientos on 5/9/2017.
+ */
+public class DrawablePoint extends Point{
+    private final Point point;
+    private ConvexHullPanel container;
+
+    public DrawablePoint(Point point)
+    {
+        this.point = point;
+    }
+
+    public void draw(Graphics g)
+    {
+        Point center = container.getCenter();
+        g.drawOval(point.getX() + center.getX(), center.getY() - point.getY(), 3, 3);
+    }
+
+    public void setContainer(ConvexHullPanel container) {
+        this.container = container;
+    }
+}
